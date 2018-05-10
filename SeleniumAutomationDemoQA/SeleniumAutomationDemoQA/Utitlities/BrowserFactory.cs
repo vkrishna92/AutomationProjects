d93c0600a9c9770c7dba107f480854ff0caa8552
vkrishna92 <32779061+vkrishna92@users.noreply.github.com>
2018-05-09 23:44:56 -0700

    code changes

---
+++ SeleniumAutomationDemoQA/SeleniumAutomationDemoQA/Utitlities/BrowserFactory.cs
@@ -0,0 +1,41 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumAutomationDemoQA.Utitlities
+{
+    class BrowserFactory:DriverClass
+    {
+        /// <summary>
+        /// returns instance of the desired browser
+        /// </summary>
+        /// <param name="browserName"></param>
+        /// <returns></returns>
+        public static IWebDriver getInstance(string browserName)
+        {
+            if (driver == null)
+            {
+
+                if(browserName.Contains("chrome"))
+                {
+                    driver = new ChromeDriver();
+                }
+                else if(browserName.Contains("firefox"))
+                {
+                    //firefox driver 
+                }
+                else
+                {
+                    driver = new ChromeDriver();
+                }
+            }
+            return driver;
+        }
+        
+    }
+}
---
+++ SeleniumAutomationDemoQA/SeleniumAutomationDemoQA/Utitlities/TestBase.cs
@@ -0,0 +1,18 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumAutomationDemoQA.Utitlities
+{
+    /// <summary>
+    /// Implemented Singleton Pattern to get browser instance
+    /// </summary>
+    class TestBase
+    {
+        
+    }
+}
