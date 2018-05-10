eb247882a24ef24e4a26a5a0f3b4c5aed56b12e4
vkrishna92 <32779061+vkrishna92@users.noreply.github.com>
2018-05-09 23:23:15 -0700

    adding changes

---
+++ SeleniumAutomationDemoQA/SeleniumAutomationDemoQA/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumAutomationDemoQA
+{
+    public class Class1
+    {
+        //make changes here
+        //start here 
+    }
+}
