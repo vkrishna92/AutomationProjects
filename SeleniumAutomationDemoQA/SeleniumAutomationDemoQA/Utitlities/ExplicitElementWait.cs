d61cff9609109d91035b58861986de32cf95807c
vkrishna92 <32779061+vkrishna92@users.noreply.github.com>
2018-05-12 09:46:44 -0700

    singleton Patter

--- SeleniumAutomationDemoQA/SeleniumAutomationDemoQA/Utitlities/ExplicitElementWait.cs
+++ SeleniumAutomationDemoQA/SeleniumAutomationDemoQA/Utitlities/ExplicitElementWait.cs
@@ -10,36 +10,41 @@ namespace SeleniumAutomationDemoQA.Utitlities
 {
     class ExplicitElementWait:DriverClass
     {
+        private static WebDriverWait wait;
+        private static void WebDriverWait()
+        {
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(20));
+        }
         
         public static void ElementIsVisible(By elementObject)
         {
-            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(20));
+            WebDriverWait();
             wait.Until(ExpectedConditions.ElementIsVisible(elementObject));
         }
         public static void ElementExists(By elementObject)
         {
-            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(20));
+            WebDriverWait();
             wait.Until(ExpectedConditions.ElementExists(elementObject));            
         }
         public static void ElementToBeClickable(By elementObject)
         {
-            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(20));
+            WebDriverWait();
             wait.Until(ExpectedConditions.ElementToBeClickable(elementObject));
         }
         public static void ElementToBeClickable(IWebElement element)
         {
-            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(20));
+            WebDriverWait();
             wait.Until(ExpectedConditions.ElementToBeClickable(element));
         }
 
         public static void TextToBePresentInElement(IWebElement element, string element_text)
         {
-            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(20));
+            WebDriverWait();
             wait.Until(ExpectedConditions.TextToBePresentInElement(element,element_text));
         }
         public static void TextToBePresentInElementLocated(By elementObject,string element_text)
         {
-            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(20));
+            WebDriverWait();
             wait.Until(ExpectedConditions.TextToBePresentInElementLocated(elementObject,element_text));
         }
     }
